0a8994e3ff815fce2a067ff507098e982de9870e
Ben Hubsch <bah37@duke.edu>
2017-06-30 15:33:03 -0700

    Functionality to add tasks

--- reactApp/components/InputLine.js
+++ reactApp/components/InputLine.js
@@ -3,13 +3,25 @@ import React from 'react';
 class InputLine extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            typedText: ''
+        }
+    }
+
+    handleTyping(event) {
+        this.setState({typedText: event.target.value})
+    }
+
+    handleSubmit() {
+        this.props.submit(this.state.typedText)
+        this.setState({typedText: ''})
     }
 
     render() {
         return (
             <div>
-                <input type="text" placeholder="Add a task" name="addTask"></input>
-                <button type="submit" onClick={() => this.props.submit('Test Task')}>Add Todo</button>
+                <input type="text" placeholder="Add a task" onChange={(event) => this.handleTyping(event)} value={this.state.typedText}></input>
+                <button type="submit" onClick={() => this.handleSubmit()}>Add Todo</button>
             </div>
         )
     }
