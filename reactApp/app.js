12c07e0038eddfec7212aa2ef128e24e69ac05c0
Ben Hubsch <bah37@duke.edu>
2017-06-30 14:05:02 -0700

    Simple todo list rendered

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,5 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>React lives!</p>,
-   document.getElementById('root'));
+var dummyData = ['Wash the dishes', 'Take out the trash', 'Clean the keyboard', 'Call your mom']
+
+class Todo extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <li><button>X</button>{' ' + this.props.task}</li>
+            </div>
+        )
+    }
+}
+
+class TodoList extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <ul>
+                {dummyData.map(function(task) {
+                    return <Todo task={task}/>
+                })}
+            </ul>
+        )
+    }
+}
+
+ReactDOM.render(<TodoList />, document.getElementById('root'));
